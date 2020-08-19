cacc58c3699e267bc2e68470987f0bbb24ed85d9
Ming-der Wang <mingder78@yahoo.com>
2020-08-19 06:37:55 +0000

    Initial commit

---
+++ geeks.py
@@ -0,0 +1,4 @@
+class geeks:  
+    def __init__(self, name, roll):  
+        self.name = name  
+        self.roll = roll 
---
+++ list.py
@@ -0,0 +1,29 @@
+'''
+https://note.nkmk.me/en/python-function-return-multiple-values/
+
+我們用 function 可以一次回傳多個值, 當成了解 list 與 tuple 不同的地方
+'''
+
+# list 數學叫 "陣列", 其特性是 一堆有 “順序” 關係的東西清單.
+# 這些東西, 未必要同一型態, 但同一型態 的 list 比較合理(且常用)
+
+'''
+你覺得日常生活中, 有哪些是適合用 list 的變數來描述的？
+'''
+# 比如 排隊的人, 就是一種 list
+# 還有哪些? 也是 list ? 舉 2 個以上常用的例子？
+
+# > 手機通訊錄、書架上的書
+
+'還有嗎? '
+# 英文的 購物清單 叫 shopping list, 也是可以用 list 型態在程式裡使用
+
+'---------------------------------------------'
+'''
+https://www.geeksforgeeks.org/how-to-create-a-list-of-object-in-python-class/
+
+用 class 定義物件, 再把一堆 class 的 instance (物體) 加入一個清單的範例. 
+
+這是常用的方法, 多加練習
+'''
+
